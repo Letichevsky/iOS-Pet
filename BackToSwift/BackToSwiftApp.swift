977f8e85d6ee821e3cf2efccb7271f883fa8b7e6
VL <vladislavletichevsky@gmail.com>
2024-10-19 10:57:20 +0200

    Initial Commit

---
+++ BackToSwift/BackToSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackToSwiftApp.swift
+//  BackToSwift
+//
+//  Created by VL on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BackToSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
